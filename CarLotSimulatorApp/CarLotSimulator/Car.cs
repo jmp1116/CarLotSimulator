175c629ee008a804c42b14ef6c9c127cc4dc3ace
John Mark <jmp1116@gmail.com>
2020-09-18 19:29:18 -0500

    Pushed the project back up

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+
+        public Car()
+        {
+
+        }
+        public Car(string make, string model, int year, string engineNoise, string honkNoise, bool isDrivable)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDrivable = isDrivable;
+
+        }
+
+       
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDrivable { get; set; }
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine(EngineNoise);
+        }
+        
+
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine(HonkNoise);
+        }
+        
+
+
+    }
+}
